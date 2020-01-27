8c33ae24867905cbfa67ecd227b894283df23d09
gabriel <gabriel.leyba@globant.com>
2020-01-27 00:41:20 -0300

    tapioca

--- src/app/tabs/ventas/ventas.page.ts
+++ src/app/tabs/ventas/ventas.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CatalogoService } from '../../services/catalogo.service';
 import { Item } from '../../classes/item';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-ventas',
@@ -12,10 +13,14 @@ export class VentasPage implements OnInit {
   public filtro: string = "";
   public catalogo:Item[] = [];
 
-  constructor(public catalogoService:CatalogoService) { }
+  constructor(public catalogoService:CatalogoService, public platform: Platform) { }
 
   ngOnInit() {
     this.catalogo = this.catalogoService.getCatalogo();
+    this.platform.ready().then((readySource) => {
+      console.log('Width: ' + this.platform.width());
+      console.log('Height: ' + this.platform.height());
+    });
   }
 
   filtrarLista() {
